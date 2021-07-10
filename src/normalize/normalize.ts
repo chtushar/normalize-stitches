97ccf0811d4e7865747210344f1676bd462c405c
Tushar Choudhari <33191895+chtushar@users.noreply.github.com>
2021-07-10 14:29:58 +0530

    :sparkles: Complete normalize

--- src/normalize/normalize.ts
+++ src/normalize/normalize.ts
@@ -24,6 +24,8 @@ export type Definition =
  * CSS to normalize abnormalities across browsers (normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css)
  * @example
  * ```ts
+ * import { normalize } from 'modern-normalize'
+ *
  * const globalStyles = global({
  *  ...normalize,
  *  // Other custom global styling
@@ -206,5 +208,149 @@ export const normalize = {
 
   'button, [type="button"], [type="reset"], [type="submit"]': {
     appearance: 'button',
+  },
+
+  /**
+   * Remove the inner border and padding in Firefox.
+   */
+
+  'button::-moz-focus-inner, [type="button"]::-moz-focus-inner, [type="reset"]::-moz-focus-inner, [type="submit"]::-moz-focus-inner':
+    {
+      borderStyle: 'none',
+      padding: 0,
+    },
+
+  /**
+   * Restore the focus styles unset by the previous rule.
+   */
+
+  'button:-moz-focusring, [type="button"]:-moz-focusring, [type="reset"]:-moz-focusring, [type="submit"]:-moz-focusring':
+    {
+      outline: '1px dotted ButtonText',
+    },
+
+  /**
+   * Correct the padding in Firefox.
+   */
+
+  fieldset: {
+    padding: '0.35em 0.75em 0.625em',
+  },
+
+  /**
+   * 1. Correct the text wrapping in Edge and IE.
+   * 2. Correct the color inheritance from `fieldset` elements in IE.
+   * 3. Remove the padding so developers are not caught out when they zero out
+   *    `fieldset` elements in all browsers.
+   */
+
+  legend: {
+    boxSizing: 'border-box', // 1
+    color: 'inherit', // 2
+    display: 'table', // 1
+    maxWidth: '100%', // 1
+    padding: 0, // 3
+    whiteSpace: 'normal', // 1
+  },
+
+  /**
+   * Add the correct vertical alignment in Chrome, Firefox, and Opera.
+   */
+
+  progress: {
+    verticalAlign: 'baseline',
+  },
+
+  /**
+   * Remove the default vertical scrollbar in IE 10+.
+   */
+
+  textarea: {
+    overflow: 'auto',
+  },
+
+  /**
+   * 1. Add the correct box sizing in IE 10.
+   * 2. Remove the padding in IE 10.
+   */
+
+  '[type="checkbox"],[type="radio"]': {
+    boxSizing: 'border-box', // 1
+    padding: 0, // 2
+  },
+
+  /**
+   * Correct the cursor style of increment and decrement buttons in Chrome.
+   */
+
+  '[type="number"]::-webkit-inner-spin-button, [type="number"]::-webkit-outer-spin-button':
+    {
+      height: 'auto',
+    },
+
+  /**
+   * 1. Correct the odd appearance in Chrome and Safari.
+   * 2. Correct the outline style in Safari.
+   */
+
+  '[type="search"]': {
+    appearance: 'textfield', // 1
+    outlineOffset: '-2px', // 2
+  },
+
+  /**
+   * Remove the inner padding in Chrome and Safari on macOS.
+   */
+
+  '[type="search"]::-webkit-search-decoration': {
+    appearance: 'none',
+  },
+
+  /**
+   * 1. Correct the inability to style clickable types in iOS and Safari.
+   * 2. Change font properties to `inherit` in Safari.
+   */
+
+  '::-webkit-file-upload-button': {
+    appearance: 'button', // 1
+    font: 'inherit', // 2
+  },
+
+  /* Interactive
+   ========================================================================== */
+
+  /**
+   * Add the correct display in Edge, IE 10+, and Firefox.
+   */
+
+  details: {
+    display: 'block',
+  },
+
+  /**
+   * Add the correct display in all browsers.
+   */
+
+  summary: {
+    display: 'list-item',
+  },
+
+  /* Misc
+   ========================================================================== */
+
+  /**
+   * Add the correct display in IE 10+.
+   */
+
+  template: {
+    display: 'none',
+  },
+
+  /**
+   * Add the correct display in IE 10.
+   */
+
+  '[hidden]': {
+    display: 'none',
   },
 } as Definition;
